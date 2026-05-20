fb158a1bd0771b52aa92232f1c7e8d98a9903d74
Kunal singh <ks6024108@gmail.com>
2026-05-20 16:01:36 +0530

    initial designing and button tested...

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,23 @@
+import Button from "../components/ui/Button";
+import SectionTitle from "../components/common/SectionTitle";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <main>
+      <section className="section-padding">
+        <div className="container-custom">
+          <SectionTitle
+            subtitle="LeadBoost Marketing"
+            title="Premium Digital Growth Agency"
+          />
+
+          <div className="flex justify-center">
+            <Button>Get Started</Button>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
